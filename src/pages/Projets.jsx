8e1eb384395dc2f77b1fb06847b852eb25c3e88f
stephanieEt <stetourneau@laposte.net>
2024-07-03 15:04:13 +0200

    create data.json

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,9 +1,17 @@
+import { Modal, Button } from "react-bootstrap";
+import { useState } from "react";
+
 const Projets = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="container-projet container-page">
       <div className="row">
         <div className="col-md-6 card-img-projet">
-          <a href="#" target="_blank">
+          <a href="#" onClick={handleShow}>
             <img
               src="./src/assets/images/booki.png"
               className="img-thumbnail"
@@ -38,6 +46,20 @@ const Projets = () => {
             />
           </a>
         </div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   );
