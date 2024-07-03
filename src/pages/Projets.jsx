6754e5923655cb0b36c6c6e2fd4674ba90d06f2c
stephanieEt <stetourneau@laposte.net>
2024-07-03 22:54:43 +0200

    create modal card

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,65 +1,13 @@
-import { Modal, Button } from "react-bootstrap";
-import { useState } from "react";
+import datas from "../../data.json";
+import Card from "../components/card";
 
 const Projets = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
     <div className="container-projet container-page">
       <div className="row">
-        <div className="col-md-6 card-img-projet">
-          <a href="#" onClick={handleShow}>
-            <img
-              src="./src/assets/images/booki.png"
-              className="img-thumbnail"
-              alt=""
-            />
-          </a>
-        </div>
-        <div className="col-md-6 card-img-projet">
-          <a href="#" target="_blank">
-            <img
-              src="./src/assets/images/ohmyfood.png"
-              className="img-thumbnail"
-              alt=""
-            />
-          </a>
-        </div>
-        <div className="col-md-6 card-img-projet">
-          <a href="#" target="_blank">
-            <img
-              src="./src/assets/images/sophie.png"
-              className="img-thumbnail"
-              alt=""
-            />
-          </a>
-        </div>
-        <div className="col-md-6 card-img-projet">
-          <a href="#" target="_blank">
-            <img
-              src="./src/assets/images/kasa.png"
-              className="img-thumbnail"
-              alt=""
-            />
-          </a>
-        </div>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        {datas.map((data) => {
+          return <Card data={data} key={data.id} />;
+        })}
       </div>
     </div>
   );
